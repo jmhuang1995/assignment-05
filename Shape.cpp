72fccdb4db37d3ef19a0a10e2b418e215fd9050c
jmhuang1995 <justin_huang@csu.fullerton.edu>
2016-05-13 14:33:35 -0700

    Update Shape.cpp

--- Shape.cpp
+++ Shape.cpp
@@ -1,1 +1,45 @@
 
+#include "Point.h"
+#include "Shape.h"
+ using std::abs;
+
+Rectangle::Rectangle(Point middle, double recWidth, double recHeight)
+ : middle(middle), recWidth(recWidth), recHeight(recHeight) {}
+
+
+bool Rectangle::contains(const Point & p) const {
+	return abs(p.getWidth() - middle.getWidth()) <= recWidth &&
+		abs(p.getHeight() - middle.getHeight()) <= recHeight;
+	
+}
+
+
+Square::Square(Point middle, double side) : Rectangle(middle, side / 2, side / 2) {}
+
+
+Ellipse::Ellipse(Point focal1, Point focal2, double radius)
+: focal1(focal1), focal2(focal2), radius(radius) {}
+
+bool Ellipse::contains(const Point & p) const {
+	if (focal1.distance(p) + focal2.distance(p) <= radius)
+	{ return true;
+	}
+	else
+	{ return true;
+	}
+	
+}
+
+
+Circle::Circle(Point center, double) : center(center), radius(radius) {}
+
+bool Circle::contains(const Point & p) const 
+{
+	if (radius <= center.distance(p))
+	{ return true;
+	}
+	else
+	{ return false;
+	}
+	
+}
